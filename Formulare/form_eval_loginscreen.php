cc175dc1013028f646b35e2e05d8328be10a8730
Turing1887 <tim.zschage@t-online.de>
2016-06-14 18:24:27 +0200

    Funktionalitaeten des LoginScreens
    Funktionen des LoginScreens hinzugefuegt. User kann sich jetzt
    entsprechend einloggen

--- Formulare/form_eval_loginscreen.php
+++ Formulare/form_eval_loginscreen.php
@@ -1,9 +1,32 @@
 <?php 
 
-
-
-
-
-
+	if($_POST["username"] =="admin"){
+		if($_POST["password"]=="password"){
+			
+			
+			header("Location: PlayerCountScreen.html");
+			
+		}
+		else{
+			echo 'Username or Password invalid';
+			
+		}
+		
+	}
+	
 
 ?>
+
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+	
+    </body>
+</html>
