df60bca46e81f789fe839426c1777199f363682d
Turing1887 <tim.zschage@t-online.de>
2016-08-26 20:29:05 +0200

    Update login form
    the php file reads from the 'user-file.txt' now and evaluates the
    username and password you registered with

--- Formulare/form_eval_loginscreen.php
+++ Formulare/form_eval_loginscreen.php
@@ -1,19 +1,63 @@
-<?php 
+<?php
+
+	//$found = false;
+	$file = fopen('user-file.txt','r');
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$file_array = file('user-file.txt');
+	$array_length = count($file_array);
+	$success = false;
+
+
+	foreach ($file_array as $user) {
+		$user_details = explode('|', $user);
+		if ($user_details[0] == $username && $user_details[1] == $password) {
+        $success = true;
+        break;
+    }
+	}
+	if($success == true){
+		header("Location: ../HTML/PlayerCountScreen.html");
+	}
+	else{
+		echo "err";
+	}
+
+/*
+	while(!$found){
+		$line = fgets($file);
+		if($line == $username){
+			$nextline = fgets($file);
+			if($nextline == $password){
+				fclose($file);
+				$found = true;
+				header('Location: ../HTML/PlayerCountScreen.html');
+			}
+			else if(feof($file)){
+				fclose($file);
+				echo "<p>Password is wrong</p>";
+				$found = true;
+			}
+		}
+		else if(feof($file)){
+			fclose($file);
+			echo "<p>Username is wrong</p>";
+			$found = true;
+		}
+	}
+
 
 	if($_POST["username"] =="admin"){
 		if($_POST["password"]=="password"){
-			
-			
-			header("Location: PlayerCountScreen.html");
-			
+			header("Location: ../HTML/PlayerCountScreen.html");
 		}
 		else{
 			echo 'Username or Password invalid';
-			
+
 		}
-		
-	}
-	
+
+	}*/
+
 
 ?>
 
@@ -24,9 +68,9 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title></title>
+        <title>Err</title>
     </head>
     <body>
-	
+
     </body>
 </html>
