89357d6488fe9be0124f4a6a925a49ad8882bf0a
Turing1887 <tim.zschage@t-online.de>
2016-08-26 20:23:00 +0200

    Update register form
    if you create a username and a password, the code creates a textfile
    with the information

---
+++ Formulare/form_eval_register.php
@@ -0,0 +1,34 @@
+<?php
+$new_user;
+$password;
+$cookie_name = $new_user;
+
+  if($_POST["password"] != $_POST["password-repeat"]){
+    echo "<h3>Passwords don't match</h3>";
+  }
+  else{
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $new_user = $username . "|" . $password;
+
+    $userfile = fopen('user-file.txt','a') or die ("unable to open file");
+    fwrite($userfile,$new_user);
+    //fwrite($userfile,$password . "\n");
+    fclose($userfile);
+
+    header('Location: ../HTML/LoginScreen.html');
+  }
+
+
+ ?>
+ <!DOCTYPE html>
+
+ <html lang="en">
+     <head>
+         <meta charset="utf-8" />
+         <title>Err</title>
+     </head>
+     <body>
+
+     </body>
+ </html>
