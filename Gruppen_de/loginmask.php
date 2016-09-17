ee2401ce97f2dc29593047862ff47b31a557b0af
Turing1887 <tim.zschage@t-online.de>
2016-09-17 20:31:50 +0200

    Created loginmask.php

---
+++ Gruppen_de/loginmask.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Kingsburg</title>
+        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+        <link rel="icon" href="../favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+      <label for="username">Username: </label><br>
+      <input type="text" name="username" placeholder="username" required><br> <!--Eingabe des usernames -->
+      <label for="password">Password: </label><br>
+      <input type="password" name="password" placeholder="password" required><br><!--Eingabe des Passworts-->
+    </body>
+</html>
