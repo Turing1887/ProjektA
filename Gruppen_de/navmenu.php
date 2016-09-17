03f89888bcbb93ec842ff69f32c55f556233e4d5
Turing1887 <tim.zschage@t-online.de>
2016-09-17 20:18:27 +0200

    Created navmenu.php

---
+++ Gruppen_de/navmenu.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Kingsburg</title>
+        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+        <link rel="icon" href="../favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+      <nav>
+        <a href="view_loginscreen.html">Ausloggen</a>
+        <a href="impressum.php" target="_blank">Impressum</a>
+        <a href="https://www.fantasyflightgames.com/en/index/" target="_blank">Website</a>
+        <a href="https://images-cdn.fantasyflightgames.com/filer_public/01/74/0174d10b-b406-44d8-ab7e-f9a6d31046b4/kingsburg-rules.pdf" target="_blank">Game manual</a>
+      </nav>
+    </body>
+</html>
