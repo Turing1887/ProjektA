5882d32baed851455b15360511769520af366240
Turing1887 <tim.zschage@t-online.de>
2016-09-17 13:10:22 +0200

    Created impressum.php
    * added information about the company
    * added link to original website

---
+++ Views/impressum.php
@@ -0,0 +1,28 @@
+<?php ?>
+<!DOCTYPE html>
+
+ <html lang="en">
+     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Kingsburg</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+        <link rel="icon" href="../favicon.ico" type="image/x-icon">
+     </head>
+     <body>
+       <section>
+         <p>
+           <strong>Fantasy Flight Games</strong><br>
+           1995 County Road B2 West<br>
+           Roseville, MN 55113<br>
+           United States of America<br>
+           Phone: 855-382-8880<br>
+           Fax: 651-639-1764<br>
+        </p>
+        <p>© 2016 Fantasy Flight Publishing, Inc. Fantasy Flight Games and the FFG logo are ® of Fantasy Flight Publishing, Inc. All rights reserved.</p>
+        <a href="https://www.fantasyflightgames.com/en/index/">https://www.fantasyflightgames.com</a>
+       </section>
+     </body>
+ </html>
