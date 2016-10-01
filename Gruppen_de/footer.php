6874e904faaef04079c53aff4605ec791e459679
Turing1887 <tim.zschage@t-online.de>
2016-10-01 14:00:56 +0200

    Upload footer.php and ingame_menu.php
    * new footer group

---
+++ Gruppen_de/footer.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Kingsburg</title>
+        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+        <link rel="icon" href="../favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+      <nav>
+
+        <a href="../Views/impressum.php" target="_blank">Impressum</a>
+        <a href="https://www.fantasyflightgames.com/en/index/" target="_blank">Website</a>
+
+      </nav>
+    </body>
+</html>
---
+++ Gruppen_de/ingame_menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Kingsburg</title>
+        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+        <link rel="icon" href="../favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+      <nav>
+          <a href=""><button id="saveButton">Save</button></a>
+          <a href="https://images-cdn.fantasyflightgames.com/filer_public/01/74/0174d10b-b406-44d8-ab7e-f9a6d31046b4/kingsburg-rules.pdf" target="_blank"><button>Game manual</button></a>
+          <a href="view_startbildschirm.html"><button id="quitButton">Quit</button></a>
+          <a href="view_loginscreen.php"><button>Ausloggen</button></a>
+
+
+      </nav>
+    </body>
+</html>
