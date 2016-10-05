eef39fede95a6491d6e834a954196a932fe949c7
Turing1887 <tim.zschage@t-online.de>
2016-10-05 16:38:59 +0200

    added button and include
    * added a button to start the game
    * added include statements to include groups

--- Views/view_startbildschirm.php
+++ Views/view_startbildschirm.php
@@ -8,8 +8,11 @@
         <link rel="icon" href="../favicon.ico" type="image/x-icon">
     </head>
     <body>
+      <?php include("../Gruppen_de/ingame_menu.php"); ?>
       <h1>Click on the Game to start</h1>
-      <a href="view_spielmodi.php"><img src="kingsburg.jpg" alt="Kingsburg" style="height: 600px;width: 800px;"></a><br> <!--Cover des Spiels als Startbutton -->
+      <img src="kingsburg.jpg" alt="Kingsburg" style="height: 600px;width: 800px;"><br> <!--Cover des Spiels als Startbutton -->
+      <a href="view_spielmodi.php"><button id="startButton">New Game</button></a><br>
       <input id="loadButton" type="button" value="Load Game"><!--Laden des Spielstands-->
+      <?php include("../Gruppen_de/footer.php"); ?>
     </body>
 </html>
