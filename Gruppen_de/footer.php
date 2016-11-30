271060441dae62c89f650829de797abb6f0f3f06
Tim Zschage <Tim.Zschage@global.com>
2016-11-30 11:07:23 +0100

    Small design change

--- Gruppen_de/footer.php
+++ Gruppen_de/footer.php
@@ -1,21 +1,8 @@
-<?php
-
-?>
-<!DOCTYPE html>
-
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>Kingsburg</title>
-        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
-        <link rel="icon" href="../favicon.ico" type="image/x-icon">
-    </head>
-    <body>
-      <nav>
-
-        <a href="../Views/impressum.php" target="_blank">Impressum</a>
-        <a href="https://www.fantasyflightgames.com/en/index/" target="_blank">Website</a>
-
-      </nav>
-    </body>
-</html>
+  <link rel="stylesheet" type="text/css" href="../css/footer.css">
+  <footer>
+  <ul>
+    <li> <a href="../Views/impressum.php" target="_blank">Impressum</a></li>
+    <li> <a href="https://www.fantasyflightgames.com/en/index/" target="_blank">Website</a></li>
+  </ul>
+  </footer>
+   
--- Views/view_loginscreen.php
+++ Views/view_loginscreen.php
@@ -9,6 +9,9 @@
         <link rel="stylesheet" type="text/css" href="../css/login.css"> 
     </head>
     <body>
+    <div class="headline">
+        <h1>Kingsburg</h1>
+    </div>
     <section class="login-form">
         <form action="../Formulare/form_eval_loginscreen.php" method="post">  
             <h2>Please enter username and password</h2>
